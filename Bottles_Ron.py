bd4f3597952e4143f63c6f6b38865721034ab71e
user <user@user-PC.(none)>
2015-03-11 18:12:33 +0100

    Ejercicio canción Bottles Ron

---
+++ Bottles_Ron.py
@@ -0,0 +1,12 @@
+''' Autores: David Blanco Fuentes
+	     Ildefonso de la Cruz Romero
+'''
+
+
+for i in range(99,0,-1):
+    print i ," bottles of beer on the wall, " , i, " bottles of beer."
+    print "Take one down, pass it around. " ,(i-1), " bottles of beer on the wall."
+    print ""
+
+
+
